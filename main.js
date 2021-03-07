de669befca3539dc2a9361f740af3fddc7afef3b
Jiří Kořenek <jirklym@gmail.com>
2021-03-08 00:22:06 +0100

    Donate Added
    Donate tlacitko pridano

--- main.js
+++ main.js
@@ -53,6 +53,12 @@ alert_button.addEventListener("click", function(useAlert,alrBtn){
 
 nick = ""
 
+rndDiv = main_window.document.createElement("div");
+
+rndDiv.innerHTML = '<form action="https://www.paypal.com/donate" method="post" target="_top"><input type="hidden" name="business" value="jirklym@gmail.com" /><input type="hidden" name="item_name" value="Buy me coffe 😂" /><input type="hidden" name="currency_code" value="USD" /><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_CZ/i/scr/pixel.gif" width="1" height="1" /></form>';
+
+bodyTag[0].appendChild(rndDiv);
+
 
 
 
